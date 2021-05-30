74ae41722bcbee8374722502ac86cc9d7ceba3f6
Pierre450 <pierre@pnhl.com>
2021-05-30 15:58:59 -0500

    Create azlogs.ps1

---
+++ Azure/azlogs.ps1
@@ -0,0 +1,24 @@
+# If not already installed, you will need the Az module:
+# 	Install-Module –Name Az -AllowClobber
+# 	Import-Module Az; Get-Module Az
+# Connect to your Azure account and if you have multiple subscriptions, selected the appropriate one:
+# 	Connect-AzAccount
+# 	Get-AzSubscription
+# 	Set-AzContext -Subscription <name of subscription>
+
+
+# By default get-azlog will only show 1000 events that took place 7 days from the current date/time
+# You may want to specific a specific date range, example: -StartTime 2021-03-31T00:00 -EndTime 2021-04-02T00:00
+
+$results = get-azlog -ResourceProvider "Microsoft.Compute" -DetailedOutput -StartTime 2021-03-31T00:00 -EndTime 2021-04-02T00:00
+$results.Properties | foreach {$_} | foreach {
+   $contents = $_.content
+   if ($contents -and $contents.ContainsKey("responseBody")) {
+      $fromjson=($contents.responseBody | ConvertFrom-Json)
+      $newobj = New-Object psobject
+      $newobj | Add-Member NoteProperty VmId $fromjson.properties.vmId
+      $newobj | Add-Member NoteProperty Vmsize $fromjson.properties.hardwareprofile.vmsize
+      $newobj
+      }
+}
+
