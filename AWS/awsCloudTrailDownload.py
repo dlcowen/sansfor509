de066b0372a31070e20f6f7da429a7f4bcfcbb4a
David Cowen <dlcowen@gmail.com>
2022-09-07 18:20:16 -0500

    Update awsCloudTrailDownload.py

--- AWS/awsCloudTrailDownload.py
+++ AWS/awsCloudTrailDownload.py
@@ -2,7 +2,8 @@
 # AWS Default Cloudtrail Download script for FOR509
 # This script will dump the last 90 days of CloudTrail logs from the AWS maintained trail
 # For org created trails in buckets you will need to download that bucket
-# Copyright: David Cowen 2021
+# V2 of this script now iterates through all regions
+# Copyright: David Cowen 2022
 
 from __future__ import print_function
 import boto3, argparse, os, sys, json, time
@@ -23,33 +24,47 @@ def main(args):
 
     # Begin permissions enumeration
 
+
+    cloudTraillogs = open("90days.json","w")
     client = boto3.client(
+        'ec2',
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+        )
+    total_logs = 0
+    regions = client.describe_regions()
+    
+    for r in regions['Regions']:
+        print("Current region:",r['RegionName'])
+        client = boto3.client(
         'cloudtrail',
         aws_access_key_id=access_key_id,
         aws_secret_access_key=secret_access_key,
-        aws_session_token=session_token
-    )
-    paginator = client.get_paginator('lookup_events')
+        aws_session_token=session_token,
+        region_name=r['RegionName']
+        )
+        paginator = client.get_paginator('lookup_events')
 
-    StartingToken = None
-    cloudTraillogs = open("90days.json","w")
-    total_logs = 0
-    page_iterator = paginator.paginate(
-        LookupAttributes=[],
-        PaginationConfig={'PageSize':50, 'StartingToken':StartingToken })
-    for page in page_iterator:
-        for event in page["Events"]:
-            cloudTraillogs.write(str(event))
-            
-            
-        try:
-            token_file = open("token","w") 
-            token_file.write(page["NextToken"]) 
-            StartingToken = page["NextToken"]
-        except KeyError:
-            exit()
-        print("Total Logs downloaded: ",total_logs)
-        total_logs = total_logs +50
+        StartingToken = None
+        
+        
+        page_iterator = paginator.paginate(
+            LookupAttributes=[],
+            PaginationConfig={'PageSize':50, 'StartingToken':StartingToken })
+        for page in page_iterator:
+            for event in page["Events"]:
+                cloudTraillogs.write(str(event))
+                
+                
+            try:
+                token_file = open("token","w") 
+                token_file.write(page["NextToken"]) 
+                StartingToken = page["NextToken"]
+            except KeyError:
+                exit()
+            print("Total Logs downloaded: ",total_logs)
+            total_logs = total_logs +50
 
 
 if __name__ == '__main__':
