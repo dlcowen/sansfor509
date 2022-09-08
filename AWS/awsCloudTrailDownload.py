372fe912eebd16157ce53921f1b5e8076db5ae81
David Cowen <dlcowen@gmail.com>
2022-09-08 13:31:52 -0500

    Update awsCloudTrailDownload.py

--- AWS/awsCloudTrailDownload.py
+++ AWS/awsCloudTrailDownload.py
@@ -30,6 +30,7 @@ def main(args):
         aws_access_key_id=access_key_id,
         aws_secret_access_key=secret_access_key,
         aws_session_token=session_token,
+        region_name="us-east-1"
         )
     total_logs = 0
     regions = client.describe_regions()
