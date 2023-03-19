ce0b85d879d0e4ec661119d701e929d3d36a2941
Pierre450 <Pierre450@users.noreply.github.com>
2023-03-19 14:44:41 -0500

    Extract WADWindowsEventLogsTable
    Powershell script to extract WADWindowsEventLogsTable

---
+++ Azure/Extract-RawXmlFromTable.ps1
@@ -0,0 +1,22 @@
+# Install the AzTable module if needed
+Install-Module AzTable
+
+# Replace the values in these variables with your own:
+$storageAccountName = "<Enter your storage account name>"
+$storageAccountKey = "<Enter the key for your storage account name>"
+$tableName = "WADWindowsEventLogsTable"
+$columnName = "RawXml"
+
+# Connect to Azure Storage using the storage account name and key
+$context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+
+# Create pointer to WADWindowsEventLogsTable
+$storageTable = Get-AzStorageTable -Name $tableName -Context $context
+$cloudTable = $storageTable.CloudTable
+
+# Read the table
+$entry=Get-AzTableRow -table $cloudTable | ft
+
+# Extract the RawXml column to a text file
+$entry.RawXml | out-file "WADWindowsEventLogsTable.xml" -encoding utf8
+
